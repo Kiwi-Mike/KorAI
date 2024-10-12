7c98272c36eaf7b2b5ecd90c409bedc1a8bff2f2
Mike Franklin <kiwimike19@gmail.com>
2024-10-12 13:55:03 +1300

    added clerk, secret keys

--- middleware.ts
+++ middleware.ts
@@ -17,3 +17,4 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import '@/styles/globals.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <Component {...pageProps} />
     </ClerkProvider>
-  );
+  )
 }
